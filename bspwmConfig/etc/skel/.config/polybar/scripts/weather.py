c89acb3f217e8f83f58f5aadb77cfd3c2bb2090a
Igor <tranzistor_2000@ukr.net>
2021-11-13 22:24:27 +0200

    Added config for bspwm

---
+++ bspwmConfig/etc/skel/.config/polybar/scripts/weather.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Procedure
+# Surf to https://openweathermap.org/city
+# Fill in your CITY
+# e.g. Antwerp Belgium
+# Check url
+# https://openweathermap.org/city/2803138
+# you will the city code at the end
+# create an account on this website
+# create an api key (free)
+# LANG included thanks to krive001 on discord
+
+
+import requests
+
+CITY = "703448"
+API_KEY = "df4f7e310a48fdebffaf08b4577942a4"
+UNITS = "Metric"
+UNIT_KEY = "C"
+#UNIT_KEY = "F"
+LANG = "uk"
+#LANG = "nl"
+#LANG = "hu"
+
+REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+try:
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Error: Unable print the data")
