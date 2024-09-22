acdebf90a1612472a933e8082dcae5796590b028
Ihor Krasylivskyi <tranzistor_2000@ukr.net>
2024-09-22 23:54:57 +0300

    Added dinnamon config

---
+++ cinnamonConfig/etc/skel/.local/share/light-mode.d/cinnamon.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+GTK_THEME="Mojave-Light"
+ICON_THEME="Papirus-Light"
+
+gsettings set org.gnome.desktop.interface color-scheme prefer-light &
+gsettings set org.x.apps.portal color-scheme prefer-light &
+gsettings set org.cinnamon.desktop.interface gtk-theme $GTK_THEME &
+gsettings set org.cinnamon.desktop.interface icon-theme $ICON_THEME &
+gsettings set org.gnome.desktop.interface gtk-theme $GTK_THEME &
+gsettings set org.gnome.desktop.interface icon-theme $ICON_THEME &
+gsettings set org.cinnamon.theme name $GTK_THEME &
