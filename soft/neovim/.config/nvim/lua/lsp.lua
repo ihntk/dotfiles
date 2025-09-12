701e4ca5d00d14b5fe5b44f315b4cf942a0eeb2f
Ihor Krasylivskyi <tranzistor_2000@ukr.net>
2025-09-12 22:59:04 +0300

    Added lsp config to nvim

---
+++ soft/neovim/.config/nvim/lua/lsp.lua
@@ -0,0 +1,75 @@
+vim.lsp.enable({'clangd', 'luals'})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set('i', '<C-Space>', function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end,
+})
+
+-- Diagnostics {{{
+local config = {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+  },
+  virtual_text = {
+    current_line = true,
+  },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "single",
+    source = "always",
+    header = "",
+    prefix = "",
+    suffix = "",
+  },
+}
+vim.diagnostic.config(config)
+-- }}}
+
+-- Improve LSPs UI {{{
+local icons = {
+  Class = " ",
+  Color = " ",
+  Constant = " ",
+  Constructor = " ",
+  Enum = " ",
+  EnumMember = " ",
+  Event = " ",
+  Field = " ",
+  File = " ",
+  Folder = " ",
+  Function = "󰊕 ",
+  Interface = " ",
+  Keyword = " ",
+  Method = "ƒ ",
+  Module = "󰏗 ",
+  Property = " ",
+  Snippet = " ",
+  Struct = " ",
+  Text = " ",
+  Unit = " ",
+  Value = " ",
+  Variable = " ",
+}
+
+local completion_kinds = vim.lsp.protocol.CompletionItemKind
+for i, kind in ipairs(completion_kinds) do
+  completion_kinds[i] = icons[kind] and icons[kind] .. kind or kind
+end
+-- }}}
