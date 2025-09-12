7b0299335e118f59e8614a86a344c6b9f7438642
Ihor Krasylivskyi <tranzistor_2000@ukr.net>
2025-09-12 22:58:12 +0300

    Added new plugins to neovim

---
+++ soft/neovim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,15 @@
+return{
+  "folke/which-key.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
